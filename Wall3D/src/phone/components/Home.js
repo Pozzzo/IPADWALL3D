003baa1f6eb79f95da2bb649f94dbffddec91c9b
Juan Pablo Castilla Yturbe <pcastillay@gmail.com>
2024-10-10 15:58:23 -0500

    Actualizacion2

--- Wall3D/src/phone/components/Home.js
+++ Wall3D/src/phone/components/Home.js
@@ -1,3 +1,18 @@
+$scope.inicio=function(){
+  $timeout(function(){
+    $scope.view.wdg['temperatureDisplay']['text']=$scope.app.mdl['Wall_Helper'].properties['Temp1']
+    $scope.view.wdg['dt-label-1']['text']=$scope.app.mdl['Wall_Helper'].properties['Temp2']
+    $scope.view.wdg['pressureDisplay']['text']=$scope.app.mdl['Wall_Helper'].properties['VFD_current']
+    $scope.view.wdg['dt-label-2']['text']=$scope.app.mdl['Wall_Helper'].properties['VFD_Speed_Read']
+    $scope.view.wdg['dt-label-3']['text']=$scope.app.mdl['Wall_Helper'].properties['VFD_torque']
+    $scope.view.wdg['modelItem-13']['color']=$scope.app.mdl['Wall_Helper'].properties['Baliza_Roja_1']
+    $scope.view.wdg['modelItem-23']['color']=$scope.app.mdl['Wall_Helper'].properties['Baliza_Verde_1']
+    $scope.view.wdg['modelItem-24']['color']=$scope.app.mdl['Wall_Helper'].properties['Baliza_Roja_2']
+    $scope.view.wdg['modelItem-25']['color']=$scope.app.mdl['Wall_Helper'].properties['Baliza_Verde_2']
+  }
+           ,500);
+}
+angular.element(ready).document($scope.inicio)
 $scope.$watch("app.mdl['Wall_Helper'].events['DataChange.Baliza_Roja_1']['eventData'][0]['newValue'][0].value", function(){
   if($scope.app.mdl['Wall_Helper'].events['DataChange.Baliza_Roja_1']['eventData'][0]['newValue'][0].value==true){
     $scope.view.wdg["modelItem-13"]["color"]="rgb(254,10,10)";
